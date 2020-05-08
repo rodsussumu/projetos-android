86a38134e0d9222a9ec46cf6cf8c1a7fd739cf05
Rodrigo Tanaka <rstanaka@aluscseduca.com.br>
2020-05-08 20:55:13 -0300

    Aula 08/05

---
+++ LeituraSMS/app/src/main/java/br/com/fiap/leiturasms/MainActivity.java
@@ -0,0 +1,23 @@
+package br.com.fiap.leiturasms;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+        if( permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, 10);
+        };
+    }
+}
